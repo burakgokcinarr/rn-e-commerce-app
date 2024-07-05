cff7bf53943fa262b7607d8edbf6999f496664c7
Burak Gökçınar <burakgokcinar@gmail.com>
2024-07-05 17:07:00 +0300

    Dummy data edited

--- src/dummy/Data.js
+++ src/dummy/Data.js
@@ -1,22 +1,24 @@
 export const Data = [
     {
         id: 1,
+        image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1694712318/Croma%20Assets/Communication/Wearable%20Devices/Images/300676_0_logazn.png?tr=w-400",
         title: "Apple Watch SE Gen 2",
         subtitle: "With solo loop",
         category: "Apple",
         colors: ["Silver", "Bright Orange", "Starlight", "Black", "Red"],
-        detail: "The aluminium case is lightweight and made from 100 percent recycled aerospace grade alloy",
+        detail: "All the essentials to help you be motivated and active, keep connected, track your health, and stay safe. The Smart Stack and redesigned apps in watchOS 10 help you see more information at a glance. With features like Crash Detection and enhanced workout metrics, Apple Watch SE is a better value than ever.",
         price: '$349.99',
         star: '4.9',
         review: []
     },
     {
         id: 2,
+        image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1691134303/Croma%20Assets/Wearable/Wearable%20Devices/Images/275944_hmcg6b.png?tr=w-400",
         title: "Galaxy Watch 4",
         subtitle: "With solo loop",
         category: "Samsung",
         colors: ["Silver", "Starlight", "Black", "Red"],
-        detail: "The aluminium case is lightweight and made from 100 percent recycled aerospace grade alloy",
+        detail: "Designed with a generous and vibrant 1.5-inch AMOLED display, the SAMSUNG Galaxy Watch6 Classic Smartwatch offers a convenient and speedy way to check notifications, apps, and other features. What’s more, the combination of a round dial and sleek design imparts a touch of stylish flair and elevates your overall outfit.",
         price: '$249.99',
         star: '4.1',
         review: []
@@ -24,21 +26,23 @@ export const Data = [
     {
         id: 3,
         title: "Amazfit GTS 2",
+        image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1706882831/Croma%20Assets/Communication/Wearable%20Devices/Images/302168_0_rqwajf.png?tr=w-400",
         subtitle: "With solo loop",
         category: "Xiaomi",
         colors: ["Silver", "Blue", "Black", "Red"],
-        detail: "The aluminium case is lightweight and made from 100 percent recycled aerospace grade alloy",
+        detail: "Immerse yourself in the vibrant 1.69-inch HD touch display of the amazfit Bip 3 smartwatch. The large and colorful screen enhances visibility, making it easy to navigate through your watch functions, notifications, and more. Enjoy a crystal-clear visual experience on your wrist.",
         price: '$199.99',
         star: '3.7',
         review: []
     },
     {
         id: 4,
-        title: "Galaxy Watch 7",
+        image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1675187472/Croma%20Assets/Communication/Wearable%20Devices/Images/260632_0_gkm677.png?tr=w-400",
+        title: "Galaxy Watch 5 Pro",
         subtitle: "With solo loop",
         category: "Samsung",
         colors: ["Silver", "Starlight", "Black", "Red", "White", "Blue"],
-        detail: "The aluminium case is lightweight and made from 100 percent recycled aerospace grade alloy",
+        detail: "Designed with a generous and vibrant 1.5-inch AMOLED display, the SAMSUNG Galaxy Watch6 Classic Smartwatch offers a convenient and speedy way to check notifications, apps, and other features. What’s more, the combination of a round dial and sleek design imparts a touch of stylish flair and elevates your overall outfit.",
         price: '$239.99',
         star: '4.8',
         review: []
@@ -46,6 +50,7 @@ export const Data = [
     {
         id: 5,
         title: "Apple Watch 8",
+        image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1685967383/Croma%20Assets/Communication/Wearable%20Devices/Images/262076_lfbftp.png?tr=w-400",
         subtitle: "With solo loop",
         category: "Apple",
         colors: ["Silver", "Bright Orange", "Starlight", "Black", "Red"],
@@ -57,32 +62,35 @@ export const Data = [
     {
         id: 6,
         title: "Galaxy Watch 5",
+        image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1703081985/Croma%20Assets/Communication/Wearable%20Devices/Images/275934_0_phlr5b.png?tr=w-400",
         subtitle: "With solo loop",
         category: "Samsung",
         colors: ["Silver", "Starlight", "Black", "Red"],
-        detail: "The aluminium case is lightweight and made from 100 percent recycled aerospace grade alloy",
+        detail: "Designed with a generous and vibrant 1.5-inch AMOLED display, the SAMSUNG Galaxy Watch6 Classic Smartwatch offers a convenient and speedy way to check notifications, apps, and other features. What’s more, the combination of a round dial and sleek design imparts a touch of stylish flair and elevates your overall outfit.",
         price: '$289.99',
         star: '4.1',
         review: []
     },
     {
         id: 7,
-        title: "Amazfit GTS 3",
+        title: "Amazfit Bip 3",
+        image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1703235602/Croma%20Assets/Wearable/Wearable%20Devices/Images/257068_oqtbbp.png?tr=w-400",
         subtitle: "With solo loop",
         category: "Xiaomi",
         colors: ["Silver", "Blue", "Black", "Red"],
-        detail: "The aluminium case is lightweight and made from 100 percent recycled aerospace grade alloy",
+        detail: "Immerse yourself in the vibrant 1.69-inch HD touch display of the amazfit Bip 3 smartwatch. The large and colorful screen enhances visibility, making it easy to navigate through your watch functions, notifications, and more. Enjoy a crystal-clear visual experience on your wrist.",
         price: '$217.99',
         star: '4.0',
         review: []
     },
     {
         id: 8,
-        title: "Galaxy Watch 8 New",
+        title: "Noise Twist Go",
+        image: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1709018625/Croma%20Assets/Wearable/Wearable%20Devices/Images/305115_zlc7ed.png?tr=w-400",
         subtitle: "With solo loop",
-        category: "Samsung",
+        category: "Twist",
         colors: ["Silver", "Starlight", "Black", "Red", "White", "Blue"],
-        detail: "The aluminium case is lightweight and made from 100 percent recycled aerospace grade alloy",
+        detail: "Keep in touch effortlessly with Tru Sync-enabled Bluetooth calling and social media notifications on the noise Twist Go smartwatch. This versatile wearable is designed to keep you connected and efficient all day long. Get alerts for calls, messages, and social updates right on your wrist, keeping you informed without needing to reach for your phone.",
         price: '$309.99',
         star: '4.6',
         review: []
